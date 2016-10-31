b740c18333029bebf528cb875a96095af0200ece
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2016-10-31 20:55:11 +0530

    Adding test user to seed data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 
 insert into users (id, name, email, password) values (1, 'superadmin', 'superadmin@gmail.com', 'superadmin');
+insert into users (id, name, email, password) values (2, 'test', 'test@gmail.com', 'test');
+
